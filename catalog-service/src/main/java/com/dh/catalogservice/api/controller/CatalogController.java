776ab99bc8313c60c43e5d71f904d65fd39e6817
genesismeli <genesismeli.96@gmail.com>
2023-03-10 00:34:05 -0300

    Fixed @EnableFeignClient

--- catalog-service/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
@@ -1,7 +1,9 @@
 package com.dh.catalogservice.api.controller;
 
+import com.dh.catalogservice.api.client.IMovieServiceClient;
 import com.dh.catalogservice.api.service.CatalogService;
 import com.dh.catalogservice.domain.model.Movie;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,22 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
 
-    private final CatalogService service;
-
-    public CatalogController(CatalogService service) {
-        this.service = service;
-    }
-
+    private final IMovieServiceClient iMovieServiceClient;
 
     @GetMapping("/{genre}")
     ResponseEntity<List<Movie>> getGenre(@PathVariable String genre) {
 
-        return ResponseEntity.ok().body(service.getMovieByGenre(genre));
+        return iMovieServiceClient.getMovieByCatalog(genre);
     }
 
 }
--- catalog-service/src/main/java/com/dh/catalogservice/domain/model/Movie.java
+++ catalog-service/src/main/java/com/dh/catalogservice/domain/model/Movie.java
@@ -1,4 +1,24 @@
 package com.dh.catalogservice.domain.model;
 
-public record Movie(Long id, String name, String genre, String urlStream) {
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String genre;
+    private String urlStream;
+
 }
